c8e51ad1922b9d0984cf669a1747f95a29f96f67
nLevin13 <nir.levvin@gmail.com>
2019-03-30 22:31:50 -0700

    sentiment analysis works

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ import string
 
 # Imports the Google Cloud client library
 from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
+from google.cloud.speech import enums as speech_enums
+from google.cloud.speech import types as speech_types
+
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums as language_enums
+from google.cloud.language import types as language_types
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.abspath("cloudconfig.json")
 
@@ -25,10 +30,10 @@ with io.open(fileName, 'rb') as audio_file:
     content = audio_file.read()
     if len(args) > 2:
         pass #Here we will make it work with URLs so we can work with audio files longer than a minute
-    audio = types.RecognitionAudio(content=content)#, uri='gs://nolabel-b9748.appspot.com/fyslwymmd.flac')
+    audio = speech_types.RecognitionAudio(content=content)#, uri='gs://nolabel-b9748.appspot.com/fyslwymmd.flac')
 
-config = types.RecognitionConfig(
-    encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+config = speech_types.RecognitionConfig(
+    encoding=speech_enums.RecognitionConfig.AudioEncoding.FLAC,
     sample_rate_hertz=44100,
     language_code='en-US',
     model='default')
@@ -40,12 +45,26 @@ operation = client.long_running_recognize(config, audio)
 
 response = operation.result(timeout=150)
 """
+text = ''
 
 response = client.recognize(config, audio)
 os.system('rm ' + fileName)
 for result in response.results:
-    print('{}'.format(result.alternatives[0].transcript))
+    text += result.alternatives[0].transcript
 if (len(response.results) == 0):
-	print('sux to succ')
+    print('sux to succ')
 
 # use results to do sentiment analysis
+
+# Instantiates a client
+lang_client = language.LanguageServiceClient()
+
+document = language_types.Document(
+    content=text,
+    type=language_enums.Document.Type.PLAIN_TEXT)
+
+# Detects the sentiment of the text
+sentiment = lang_client.analyze_sentiment(document=document).document_sentiment
+
+print('{},{}'.format(sentiment.score, sentiment.magnitude))
+print('{}'.format(text))
